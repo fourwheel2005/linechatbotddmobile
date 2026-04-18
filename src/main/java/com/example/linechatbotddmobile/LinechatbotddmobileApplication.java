aff1b1a15e728d282c9f7221e6f1f8b15a806fae
fourwheel2005 <kasidit9911@gmail.com>
2026-04-18 18:23:47 +0700

    fix application starter add bcryptencoder

--- src/main/java/com/example/linechatbotddmobile/LinechatbotddmobileApplication.java
+++ src/main/java/com/example/linechatbotddmobile/LinechatbotddmobileApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
@@ -19,6 +20,11 @@ public class LinechatbotddmobileApplication {
 
 		System.out.println("Hello");
 		SpringApplication.run(LinechatbotddmobileApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 
